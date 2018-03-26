6ad425c1aca9bd0e4740bf7ec2b4b85651e43708
blazerius54 <79137837220@yandex.ru>
2018-03-26 16:54:41 +0700

    animation

--- src/components/Categ.jsx
+++ src/components/Categ.jsx
@@ -2,61 +2,55 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Payment from './Payment';
 import { connect } from 'react-redux';
-
+import AnimateHeight from 'react-animate-height';
 
 class Categ extends Component {
     constructor (props) {
         super(props);
         this.state = {
             isVisible: false,
-            containerStyle: {
-                height: 90
-            }
+            containerStyleClose: {
+                height: 90,
+            },
+            containerStyleOpen: {
+                height: 'auto'
+            },
+            height: 50
         }
     }
 
     render () {
         const { name, path } = this.props.categ;
+        const { isVisible, containerStyleClose, containerStyleOpen, height } = this.state;
         let spent = 0;
-        // this.props.categ.payments.filter(item=>{
-        //     let date = item.date;
-        //     let year = new Date(date).getFullYear();
-        //     let month = new Date(date).getMonth();
-
-        //     if(month === new Date(this.props.date).getMonth() &&
-        //         year === new Date(this.props.date).getFullYear()
-        //     ){
-        //         spent+=item.paymentAmount
-        //     }
-        // })
 
         this.props.filtredMonth[this.props.indexCat].map((item, i)=>{
             spent += item.paymentAmount;
         })
-
         
         return (
             <li className={this.props.categ.path + ' category'} 
-            style={this.state.containerStyle}
-            ref={input=>{this.myLi = input}}
+            // style={!isVisible? containerStyleClose : containerStyleOpen}
             >
+            <AnimateHeight
+          duration={ 500 }
+          height={ height } // see props documentation bellow
+        >
                 <div className='div-category-header'>
                     <Link to={path}>
                             <p>{name}: {spent}</p>
                     </Link>
                             <p onClick={()=>{
-                                this.setState({
-                                    containerStyle: {
-                                        height: 300
-                                    },
-                                    isVisible: !this.state.isVisible})}
+                                    this.setState({
+                                        isVisible: !this.state.isVisible,
+                                        height: height === 50? 'auto' : 50,
+                                    })
+                                }
                             }>
                                 o
                             </p>
                 </div>
-                {
-                    this.state.isVisible? 
-                    <div className='category-container-content'>
+                <div className='category-container-content'>
 
                     {
                         this.props.filtredMonth[this.props.indexCat].sort((a, b) => {
@@ -71,9 +65,14 @@ class Categ extends Component {
                             )
                         })
                     }
-                    </div> : ''
-                }
+                    </div>
+                {/* {
+                    this.state.isVisible? 
+                     : ''
+                } */}
+            </AnimateHeight>
             </li>
+        
         )
     }
 }
@@ -83,7 +82,5 @@ function mapStateToProps (state) {
         filtredMonth: state.filtredMonth
     } 
 }
-
-
 
 export default connect(mapStateToProps)(Categ);
